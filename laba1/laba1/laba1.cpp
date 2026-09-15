794521ab000eea2cdd3d293350a6efd19293ebf5
aleksandrurs <lifeafter123123371@gmail.com>
2026-09-15 14:32:58 +0300

    допилена функция print начата работа в мейне сделана проверка ввода

--- laba1/laba1/laba1.cpp
+++ laba1/laba1/laba1.cpp
@@ -1,23 +1,24 @@
 ﻿#include <cmath>
 #include <vector>
 #include <iostream>
+#include <string>
 using namespace std;
 struct pipeproperties {
 	string name;
 	double length;
 	double diametr;
 	bool status;
-	bool empty() {
-		return name.empty() && lenght == 0.0 && diametr == 0.0 && status == false;
+	bool empty() const {
+		return name.empty() && length == 0.0 && diametr == 0.0 && status == false;
 	}
 };
 struct stationproperties {
 	string name;
 	int allcex;
 	int workcex;
-	string specification;
-	bool empty() {
-		return name.empty() && allcex == 0 && workcex == 0 && specification.empty();
+	int specification;
+	bool empty() const {
+		return name.empty() && allcex == 0 && workcex == 0 && specification==0;
 	}
 };
 pipeproperties createpipe() {
@@ -28,7 +29,8 @@ pipeproperties createpipe() {
 	cin >> p.length;
 	cout << "Введите диаметр трубы:";
 	cin >> p.diametr;
-	p.status = True;
+	p.status = true;
+	/*bool vizov1 = true;*/
 	return p;
 }
 stationproperties createstation() {
@@ -40,19 +42,56 @@ stationproperties createstation() {
 	st.workcex = st.allcex;
 	cout << "Введите класс станции:";
 	cin >> st.specification;
+	/*bool vizov2 = true;*/
 	return st;
 }
-// хз че делать пока с этим, с функцией чек апа, мб потом сделаю две функции empty для кс и трубы
-//void checkup(const pipeproperties& p, const stationproperties& st) {
-//	cout << "Ваша труба и КС:";
-//	if (p.empty() && st.empty()) {
-//		cout << "Вы не создавали труб и КС";
-//	}
-//	if (!p.empty() && !st.empty())
-//}
-
+void print(const pipeproperties& p, const stationproperties& st) {
+	cout << "Ваша труба и КС:";
+	if (p.empty() && st.empty()) {
+		cout << "Вы не создавали труб и КС";
+	}			
+	if (!p.empty()) {
+		cout << "У вас есть труба." << endl;
+		cout << "Свойства трубы:" << endl;
+		cout << "1) Название: " << p.name << endl;
+		cout << "2) Длина: " << p.length << endl;
+		cout << "3) Диаметр: " << p.diametr << endl;
+		cout << "4) Статус работы " << p.status << endl;
+	}
+	else {
+		cout << "Вы не создали трубу";
+	}
+	if (!st.empty()) {
+		cout << "Свойства КС:" << endl;
+		cout << "1) Название: " << st.name << endl;
+		cout << "2) Количество всех цехов: " << st.allcex << endl;
+		cout << "3) Количество рабочих цехов: " << st.workcex << endl;
+		cout << "4) Статус работы " << st.specification << endl;
+	}
+	else {
+		cout << "Вы не создали КС";
+	}
+}
+stationproperties st;
+pipeproperties p;
 int main()
 {
-	cout << "Hellow World";
-	return 0;
+	while (true) {
+		cout << "1) Добавить трубу" << endl;
+		cout << "2) Добавить КС" << endl;
+		cout << "3) Просмотр всех объектов" << endl;
+		cout << "4) Редактировать трубу" << endl;
+		cout << "Введите число от 0 до 7";
+		int vvod;
+		cin >> vvod;
+		if (cin.good()) {
+
+			}
+		else {
+			cin.clear();
+			cin.ignore(10000000,'\n');
+			cout << "Вы ввели неправильный символ, введите число от 0 до 7";	
+			continue;
+		}
+	}
 }
